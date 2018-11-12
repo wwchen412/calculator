d8bc17adcef6c8b319d98c5cd837464826c21619
William Wei <wwei.tw@pmgi.local>
2018-11-12 19:05:20 +0800

    home

--- src/app/calculator/calculator-input/calculator-input.component.ts
+++ src/app/calculator/calculator-input/calculator-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-calculator-input',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculator-input.component.scss']
 })
 export class CalculatorInputComponent implements OnInit {
+
+  @Input() doNum;
 
   constructor() { }
 
--- src/app/calculator/calculator-output/calculator-output.component.ts
+++ src/app/calculator/calculator-output/calculator-output.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-calculator-output',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorOutputComponent implements OnInit {
 
+  @Input()
+  exportNum;
   constructor() { }
 
   ngOnInit() {
